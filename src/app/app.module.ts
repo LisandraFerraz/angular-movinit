9f45112f30a92d3762f6181387246d2850ca7b14
Lisandra Ferraz de Santana <lisandraferraz.s@hotmail.com>
2022-12-27 16:18:42 -0300

    config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,13 +17,10 @@ import { AuthComponent } from './admin/auth/auth.component';
     SidebarComponent,
     MenuComponent,
     BodyContentComponent,
-    AuthComponent
+    AuthComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/utils/environment/tmdb-environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiKey: '2f03702b7c188bdee0bb59a265acd9bc',
+};
