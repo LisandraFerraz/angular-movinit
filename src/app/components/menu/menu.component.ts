e989951986b88a21375d2e80caf3336e39c162c2
Lisandra Ferraz de Santana <lisandraferraz.s@hotmail.com>
2023-01-19 00:12:53 -0300

    fixing  login screen

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { HostListener, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { NavItems, topMenuItems } from 'src/app/utils/sidebar-items';
 
 @Component({
@@ -8,6 +9,26 @@ import { NavItems, topMenuItems } from 'src/app/utils/sidebar-items';
   styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
+
+  constructor(private route: Router){}
+
+  ngOnInit() {
+    this.menuItems = topMenuItems;
+  }
+
+  hidetItems(){
+    if(this.route.url == '/login'){
+      return 'hide-items'
+    }
+    return ''
+  }
+
+  adjustItems(){
+    if(this.route.url == '/login'){
+      return 'adjust-items'
+    }
+    return ''
+  }
 
   @HostListener('window:scroll', ['$event'])
   changeMenuOnScroll(){
@@ -21,9 +42,4 @@ export class MenuComponent implements OnInit {
   }
 
   menuItems: NavItems[];
-
-  ngOnInit() {
-    this.menuItems = topMenuItems;
-  }
-
 }
